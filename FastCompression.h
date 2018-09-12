f5222e2310a2ac206065d123bf9a8c393a240591
R.J.V. Bertin <rjvbertin@gmail.com>
2018-09-12 19:50:30 +0200

    Also install the FastCompression.h header
    Committed from host : Portia.local

--- FastCompression.h
+++ FastCompression.h
@@ -5,6 +5,11 @@
  * Purpose..: Command line tool to LZVN compress a file.
  */
 
+#ifndef _FAST_COMPRESSION_H
+#define _FAST_COMPRESSION_H
+
 extern size_t lzvn_encode(void * dst, size_t dst_size, const void * src, size_t src_size, void * work_space);
 extern size_t lzvn_decode(void * dst, size_t dst_size, const void * src, size_t src_size);
 extern size_t lzvn_encode_work_size(void);
+
+#endif
